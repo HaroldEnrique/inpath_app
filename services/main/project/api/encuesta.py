16eee65bf2259dfb71abc2c86af2bef98d8c6c6d
JonathanSkills <jonathanromero@upeu.edu.pe>
2020-12-10 13:01:11 -0500

    create - list encuesta

--- services/main/project/api/encuesta.py
+++ services/main/project/api/encuesta.py
@@ -2,8 +2,8 @@
 # services/main/project/api/encuesta.pyF
 
 from flask_restful import Resource, Api
-from flask import Blueprint
-
+from flask import Blueprint, request
+from sqlalchemy import exc
 from project import db
 from project.api.model_encuesta import TipoPregunta, Pregunta
 from project.api.model_encuesta import TipoEncuesta, Encuesta, Opcion
@@ -52,6 +52,74 @@ class EncuestaList(Resource):
             'data': lista
         }
         return response_object, 200
+
+    def post(self):
+        post_data = request.get_json()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        if not post_data:
+            return response_object, 400
+
+        id_test = post_data.get('id_test')
+        id_tipo_test = post_data.get('id_tipo_test')
+        nombre_test = post_data.get('nombre_test')
+        descripcion_test = post_data.get('descripcion_test')
+        created_by = post_data.get('created_by')
+        preguntas = post_data.get('preguntas')
+
+        try:
+            if id_test == "0":
+                encuesta = Encuesta(
+                    nombre=nombre_test,
+                    descripcion=descripcion_test,
+                    estado=1,
+                    id_tipo_encuesta= id_tipo_test,
+                    created_by=created_by
+                )
+                db.session.add(encuesta)    
+                db.session.flush()
+                encuesta_id = encuesta.id
+
+
+                for i in preguntas:
+                    nom_preg = i['pregunta']
+                    tipo_pregunta = i['tipo_pregunta']
+                    tamanho = i['tamanho']
+                    opciones = i['opciones']
+
+                    pregunta = Pregunta(
+                        pregunta = nom_preg,
+                        tamanho = tamanho,
+                        id_tipo_pregunta = tipo_pregunta,
+                        id_test = encuesta_id,
+                        created_by=created_by
+                    )
+                    db.session.add(pregunta)    
+                    db.session.flush()
+                    pregunta_id = pregunta.id
+                    for j in opciones:
+                        texto = j 
+                        opcion = Opcion(
+                            texto = texto,
+                            id_pregunta = pregunta_id,
+                            created_by=created_by
+                        )
+                        db.session.add(opcion)    
+                        db.session.flush()
+                db.session.commit()
+                response_object['status'] = 'success'
+                response_object['message'] ='el test' f'{nombre_test} fue agregado!'
+            else:
+                test = Encuesta.query.filter_by(id=int(id_test)).first()
+                response_object['status'] = 'success'
+                response_object['message'] ='el test' f'{nombre_test} fue actualizado!'
+
+            return response_object, 201
+        except exc.IntegrityError:
+            db.session.rollback()
+            return response_object, 400
 
 
 api.add_resource(EncuestaList, '/encuesta')
